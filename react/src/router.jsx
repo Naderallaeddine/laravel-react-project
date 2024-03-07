93b1268c634a63b382159a0e2b0e840e4cf0a968
Naderallaeddine <naderallaeddine1@gmail.com>
2024-03-07 16:05:32 +0200

    ContextProvider

---
+++ react/src/router.jsx
@@ -0,0 +1,50 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Notfound from "./views/Notfound";
+import User from "./views/User";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./views/Dashboard";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path:'/',
+                element:<Navigate to="/user"/>
+            },
+            {
+                path: "/user",
+                element: <User />,
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+        ],
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+        ],
+    },
+
+    {
+        path: "*",
+        element: <Notfound />,
+    },
+]);
+
+export default router;
