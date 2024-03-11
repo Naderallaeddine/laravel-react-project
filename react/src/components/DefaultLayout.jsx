9e8f8f818c8a5eafbbc0e5ea3609d4b0f2669a2f
Naderallaeddine <naderallaeddine1@gmail.com>
2024-03-11 16:55:52 +0200

    add form

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { useStateContext } from '../contexts/ContextProvider'
+import React from "react";
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
-    const {user,token}=useStateContext();
-    if(!token){
-        return <Navigate to={'/login'}/>
+    const { user, token } = useStateContext();
+    if (!token) {
+        return <Navigate to={"/login"} />;
+    }
+    const onLogout=(ev)=>{
+        ev.preventDefault();
     }
 
-  return (
-    <div>
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/user">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+                    <div>
+                        {user.name}
+                        <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
+                    </div>
 
-        <Outlet/>
-    </div>
-  )
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
 }
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
+    const onSubmit=(ev)=>{
+        ev.preventDefault();
+    }
   return (
-    <div>Signup</div>
+    <div className='login-signup-form animated fadeInDown'>
+        <div className='form'>
+            <form onSubmit={onSubmit}>
+                <h1 className='title'>
+                    Register for free
+                </h1>
+                <input type='text' placeholder='Full Name'/>
+                <input type='email' placeholder='Email'/>
+                <input type='password' placeholder='Password'/>
+                <input type='password' placeholder='Password Confrmation'/>
+                <button className='btn btn-block'>Login</button>
+                <p className='message'>
+                    Already Registered? <Link to={'/login'}>SignIn</Link>
+                </p>
+            </form>
+        </div>
+
+    </div>
   )
 }
