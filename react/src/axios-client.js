cb456e10d29c5395e8fc1680da1f7ac66d80ba3c
Naderallaeddine <naderallaeddine1@gmail.com>
2024-03-13 22:10:36 +0200

    finish form

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -1,30 +1,28 @@
 import axios from "axios";
 
+
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
-});
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+})
 
-axios.interceptors.request.use((config) => {
-    const token = localStorage.getItem("ACCESS_TOKEN");
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('ACCESS_TOKEN');
+  config.headers.Authorization = `Bearer ${token}`
+  return config;
+})
 
-axios.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        try {
-            const { response } = error;
-            if (response == 401) {
-                localStorage.removeItem("ACCESS_TOKEN");
-            }
-        } catch (e) {
-            console.error(e);
-        }
-        throw error;
-    }
-);
+axiosClient.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  const {response} = error;
+  if (response.status === 401) {
+    localStorage.removeItem('ACCESS_TOKEN')
+    // window.location.reload();
+  } else if (response.status === 404) {
+    //Show not found
+  }
 
-export default axiosClient;
+  throw error;
+})
+
+export default axiosClient
--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,35 +1,58 @@
-import React from "react";
-import { Link, Navigate, Outlet } from "react-router-dom";
+import {Link, Navigate, Outlet} from "react-router-dom";
+import axiosClient from "../axios-client";
 import { useStateContext } from "../contexts/ContextProvider";
+import {useEffect} from "react";
 
 export default function DefaultLayout() {
-    const { user, token } = useStateContext();
-    if (!token) {
-        return <Navigate to={"/login"} />;
-    }
-    const onLogout=(ev)=>{
-        ev.preventDefault();
-    }
+  const {user, token, setUser, setToken, notification} = useStateContext();
 
-    return (
-        <div id="defaultLayout">
-            <aside>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/user">Users</Link>
-            </aside>
-            <div className="content">
-                <header>
-                    <div>Header</div>
-                    <div>
-                        {user.name}
-                        <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
-                    </div>
+  if (!token) {
+    return <Navigate to="/login"/>
+  }
 
-                </header>
-                <main>
-                    <Outlet />
-                </main>
-            </div>
-        </div>
-    );
+  const onLogout = ev => {
+    ev.preventDefault()
+
+    axiosClient.post('/logout')
+      .then(() => {
+        setUser({})
+        setToken(null)
+      })
+  }
+
+  useEffect(() => {
+    axiosClient.get('/user')
+      .then(({data}) => {
+         setUser(data)
+      })
+  }, [])
+
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            Header
+          </div>
+
+          <div>
+            {user.name} &nbsp; &nbsp;
+            <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+          </div>
+        </header>
+        <main>
+          <Outlet/>
+        </main>
+        {notification &&
+          <div className="notification">
+            {notification}
+          </div>
+        }
+      </div>
+    </div>
+  )
 }
